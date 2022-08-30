2e575dd814970ddceea9dd94c6d6c16ae0b94226
techkomal <104885038+techkomal@users.noreply.github.com>
2022-08-30 10:24:59 +0530

    update dashboardController.php

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -7,8 +7,10 @@ use App\Models\Post;
 
 class DashboardController extends Controller
 {
-    public function show_post(){
-        $posts = Post::all();
-        return view('home',['posts'=>$posts]);
+    public function show_post(Request $request){
+        //$posts = Post::all();
+        $userid = $request->user()->id;
+        $posts = Post::where('user_id', $userid)->get();
+        return view('dashboard',['posts'=>$posts]);
     }
 }
